d5e16d24f2e6df360ead029c05e31f0f8dab9548
Ninjavid <dborja@s.tooeletech.edu>
2022-06-24 21:19:09 -0600

    update

--- w1-d3-assignment/19-pairs-maker.js
+++ w1-d3-assignment/19-pairs-maker.js
@@ -9,7 +9,6 @@ function pairsMaker(arr) {
 		for (let j = i + 1; j < arr.length; j++) {
 			let element2 = arr[j]
 			let pairs = [element1, element2]
-			console.log(pairs + '*********************')
 			result.push(pairs);
 		}
 	}
